4eab94e922544c32907fea49dc0c8c45a5448995
Gale W <mail@galewilliams.com>
2026-05-12 15:58:53 -0400

    misc: Initial Commit

---
+++ Savannah/SavannahApp.swift
@@ -0,0 +1,21 @@
+//
+//  SavannahApp.swift
+//  Savannah
+//
+//  Created by Gale Williams on 5/12/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct SavannahApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ SavannahTests/SavannahTests.swift
@@ -0,0 +1,19 @@
+//
+//  SavannahTests.swift
+//  SavannahTests
+//
+//  Created by Gale Williams on 5/12/26.
+//
+
+import Testing
+@testable import Savannah
+
+struct SavannahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
